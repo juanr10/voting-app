1c7d34f87eb4ea78940ab46500a99c929b2acda5
Juan <argudojuan7@gmail.com>
2021-04-05 23:11:18 +0200

    Init configuration completed. Working on main app layout

---
+++ resources/views/index.blade.php
@@ -0,0 +1,5 @@
+<x-app-layout>
+    <div class="py-12">
+        Index of Ideas
+    </div>
+</x-app-layout>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600;700&display=swap">
 
         <!-- Styles -->
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
@@ -16,21 +16,68 @@
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+    <body class="font-sans bg-gray-100 text-gray-900 text-sm">
+        <header class="flex items-center justify-between px-8 py-4">
+            <a href="">
+                <img src="{{ asset("images/logo.svg") }}" alt="logo">
+            </a>
+            <div class="flex items-center">
+                @if (Route::has('login'))
+                    <div class="top-0 right-0 px-6 py-4">
+                        @auth
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
 
-            <!-- Page Heading -->
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                    {{ $header }}
-                </div>
-            </header>
+                                <a href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                    this.closest('form').submit();">
+                                    {{ __('Log out') }}
+                                </a>
+                            </form>
+                        @else
+                            <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Log in</a>
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
+                            @if (Route::has('register'))
+                                <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                            @endif
+                        @endauth
+                    </div>
+                @endif
+
+                <a href="">
+                    <img src="http://www.gravatar.com/avatar/3b3be63a4c2a439b013787725dfce802?d=identicon" alt="avatar" class="w-10 h-10 rounded-full">
+                </a>
+            </div>
+        </header>
+
+        <main class="container mx-auto max-w-custom flex">
+            <div  class="w-70 mr-5">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nihil minus illo, eligendi soluta esse officiis debitis unde illum dicta maiores.
+            </div>
+            <div class="w-175">
+                <nav class="flex items-center justify-between text-xs">
+                    <ul class="flex uppercase font-semibold border-b-4 pb-3 space-x-10">
+                        <li>
+                            <a href="#" class="border-b-4 pb-3 border-blue">All ideas (87)</a>
+                        </li>
+                        <li>
+                            <a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">Considering (6)</a>
+                        </li>
+                        <li>
+                            <a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">In progress (3)</a>
+                        </li>
+                    </ul>
+
+                    <ul class="flex uppercase font-semibold border-b-4 pb-3 border-gray space-x-10">
+                        <li>
+                            <a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">Implemented (9)</a>
+                        </li>
+                        <li>
+                            <a href="#" class="text-gray-400 transition duration-150 ease-in border-b-4 pb-3 hover:border-blue">Closed (55)</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </main>
     </body>
 </html>
