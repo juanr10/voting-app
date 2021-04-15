b38b9661a9847e4cdfe809c2e08a6947799835a8
Juan <argudojuan7@gmail.com>
2021-04-15 06:34:44 +0200

    making responsive index.blade

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -24,9 +24,9 @@
         </div>
     </div> {{-- End filters --}}
 
-    <div class="ideas-container space-y-6 my-6">
+    <div class="ideas-container space-y-6 my-8">
         <div class="idea-container flex bg-white hover:shadow-lg shadow-md rounded-xl transition duration-150 ease-in cursor-pointer">
-            <div class="border-r border-gray-100 px-5 py-8">
+            <div class="hidden md:block border-r border-gray-100 px-5 py-8">
                 <div class="text-center">
                     <div class="font-semibold text-2xl">12</div>
                     <div class="text-gray-500">Votes</div>
@@ -39,20 +39,20 @@
                 </div>
             </div>
 
-            <div class="flex flex-1 px-2 py-6">
+            <div class="flex flex-1 flex-col md:flex-row px-6 md:px-4 py-6">
                 <div class="flex-none">
                     <a href="#image">
                         <img src="https://source.unsplash.com/200x200/?face&crop=face1&v=1" alt="image-avatar" class="w-14 h-14 rounded-xl">
                     </a>
                 </div>
-                <div class="w-full mx-4">
-                    <h4 class="text-xl font-semibold">
+                <div class="w-full flex flex-col justify-between md:mx-4"">
+                    <h4 class="text-xl font-semibold mt-2 md:mt-0">
                         <a href="" class="hover:underline">A random title</a>
                     </h4>
                     <div class="text-gray-600 text-justify mt-3 line-clamp-3">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis, illo?
                     </div>
-                    <div class="flex items-center justify-between mt-6">
+                    <div class="flex flex-col justify-between md:flex-row md:items-center mt-6">
                         <div class="flex items-center text-xs text-gray-400 font-semibold space-x-2">
                             <div>10 hours ago</div>
                             <div>&bull;</div>
@@ -60,7 +60,7 @@
                             <div>&bull;</div>
                             <div class="text-gray-700">Comments</div>
                         </div>
-                        <div x-data="{ isOpen: false }" class="flex items-center space-x-2">
+                        <div x-data="{ isOpen: false }" class="flex items-center space-x-2 mt-4 md:mt-0">
                             <div class="bg-gray-200 text-xxs font-bold uppercase leading-none rounded-full text-center w-28 h-7 py-2 px-4">
                                 Open
                             </div>
@@ -85,6 +85,16 @@
                                         <a class="hover:bg-gray-100 block text-xs transition duration-150 ease-in px-5 py-3" href="">Delete Post</a>
                                     </li>
                                 </ul>
+                            </button>
+                        </div>
+
+                        <div class="flex items-center md:hidden mt-4 md:mt-0">
+                            <div class="bg-gray-100 text-center rounded-xl h-10 px-4 py-2 pr-8">
+                                <div class="text-sm font-bold leading-none">12</div>
+                                <div class="text-xxs font-semibold leading-none text-gray-400">Votes</div>
+                            </div>
+                            <button class="w-20 bg-gray-200 border border-gray-200 font-bold text-xxs uppercase rounded-xl hover:border-gray-400 transition duration-150 ease-in px-4 py-3 -mx-5">
+                                Vote
                             </button>
                         </div>
                     </div>
