540866c38d5fb3d5b2df60b9ee8435c6a04702f9
Juan <argudojuan7@gmail.com>
2021-04-21 06:49:06 +0200

    Testing show idea added

---
+++ tests/Feature/ShowIdeasTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\Idea;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class ShowIdeasTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function list_of_ideas_shows_on_main_page()
+    {
+        $ideaOne = Idea::factory()->create([
+            'title' => 'My First Idea',
+            'description' => 'Description of my first idea',
+        ]);
+
+        $ideaTwo = Idea::factory()->create([
+            'title' => 'My Second Idea',
+            'description' => 'Description of my second idea',
+        ]);
+
+        $response = $this->get(route('idea.index'));
+
+        $response->assertSuccessful();
+        $response->assertSee($ideaOne->title);
+        $response->assertSee($ideaOne->description);
+        $response->assertSee($ideaTwo->title);
+        $response->assertSee($ideaTwo->description);
+    }
+
+    /** @test */
+    public function single_idea_shows_correctly_on_the_show_page()
+    {
+        $idea = Idea::factory()->create([
+            'title' => 'My First Idea',
+            'description' => 'Description of my first idea',
+        ]);
+
+        $response = $this->get(route('idea.show', $idea));
+
+        $response->assertSuccessful();
+        $response->assertSee($idea->title);
+        $response->assertSee($idea->description);
+    }
+
+    /** @test */
+    public function same_idea_title_different_slugs()
+    {
+        $ideaOne = Idea::factory()->create([
+            'title' => 'My First Idea',
+        ]);
+        $ideaTwo = Idea::factory()->create([
+            'title' => 'My First Idea',
+        ]);
+
+        $this->assertEquals('my-first-idea', $ideaOne->slug);
+        $this->assertEquals('my-first-idea-2', $ideaTwo->slug);
+    }
+}
