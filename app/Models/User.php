69e579a964ea4d17cb66c01489a46343dd2c720c
Juan <argudojuan7@gmail.com>
2021-04-24 18:02:03 +0200

    Test for user gravatar implemented

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,16 +51,16 @@ class User extends Authenticatable
         $firstCharacter = $this->email[0];
 
         if (is_numeric($firstCharacter)) {
-            $integerToUse = ord(strtolower($firstCharacter)) - 21;
+            $defaultAvatarId  = ord(strtolower($firstCharacter)) - 21;
         } else {
-            $integerToUse = ord(strtolower($firstCharacter)) - 96;
+            $defaultAvatarId  = ord(strtolower($firstCharacter)) - 96;
         }
 
         return 'https://www.gravatar.com/avatar/'
             .md5($this->email)
             .'?s=200'
             .'&d=https://s3.amazonaws.com/laracasts/images/forum/avatars/default-avatar-'
-            .$integerToUse
+            .$defaultAvatarId
             .'.png';
     }
 }
